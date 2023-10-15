c1391c6a06b6bf81f6fb04fa4046514e3c40c25f
Aadarsh19 <111179900+Aadarsh19@users.noreply.github.com>
2023-10-15 10:36:40 +0000

    variable

---
+++ basics/variable.js
@@ -0,0 +1,12 @@
+const ac=12345
+let ae="WSAD@gmail.com"
+var ap="qwrww"
+aC="jaipur"
+
+/*
+const sirf constant k liye
+let use krnge 
+var m scope isuue
+variable bta k value nhi do to undefined variable
+*/
+console.table([ac,ae,ap,aC])
