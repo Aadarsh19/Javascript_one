195ac7e2fe54740ffc78068bea83ef8e563bf288
Aadarsh19 <111179900+Aadarsh19@users.noreply.github.com>
2023-10-17 10:05:30 +0000

    ho gyadata

---
+++ basics/datatypes.js
@@ -0,0 +1,19 @@
+let n=23;
+let isn=String(n)
+
+let isb=1;
+let check=Boolean(isb)
+let string="sfbwifbewofbeogbu"
+console.table([n,isn,isb,check,string])
+console.table([typeof n,typeof isn,typeof isb,typeof check,typeof string])
+
+//COnversion is there 
+
+console.log(3*4)
+console.log(3+ "2")
+console.log("2" + 3)
+console.log(+true)
+
+//non primitive data typeaa gye h
+let aadi=["super","duper","sbse uper"]
+console.log( aadi)
